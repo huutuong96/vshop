49d80770c57c694d374294ab626755b7272bacf9
khanghyper <khangnd1806@gmail.com>
2024-11-17 10:29:27 +0700

    1

--- src/app/(shop)/_components/new-product-variant-table.tsx
+++ src/app/(shop)/_components/new-product-variant-table.tsx
@@ -70,7 +70,7 @@ export default function NewProductVariantTable({ variantProducts, variantItems, 
                     </div>
                   </div>
                   {(errros?.variantProducts && errros?.variantProducts[index]?.price?.message) && (
-                    <span className="text-sm text-red-500">{errros?.variantProducts[index]?.price.message}</span>
+                    <span className="text-sm text-red-500">{errros.variantProducts[index].price.message}</span>
                   )}
                 </td>
                 <td className="">
