44479532d31990a2219283c691441584e5144a31
khanghyper <khangnd1806@gmail.com>
2024-11-24 23:23:48 +0700

    abx

--- src/app/test.tsx
+++ src/app/test.tsx
@@ -4,7 +4,7 @@ import { useEffect } from "react"
 
 export default function TestABX() {
   useEffect(() => {
-    fetch(`http://localhost:3000/api/auth/logout`, {
+    fetch(`/api/auth/logout`, {
       method: 'POST',
       body: JSON.stringify({})
     }).then(res => { window.location.href = '/' })
