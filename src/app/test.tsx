90d11ddb36f82ef5cd1b6116f784b15520da50a3
khanghyper <khangnd1806@gmail.com>
2024-11-24 23:26:37 +0700

    abxs

--- src/app/test.tsx
+++ src/app/test.tsx
@@ -1,14 +1,16 @@
 'use client'
 
 import LoadingScreen from "@/app/(guest)/_components/loading-screen"
+import { useRouter } from "next/navigation"
 import { useEffect } from "react"
 
 export default function TestABX() {
+  const router = useRouter();
   useEffect(() => {
     fetch(`/api/auth/logout`, {
       method: 'POST',
       body: JSON.stringify({})
-    }).then(res => { window.location.href = '/' })
+    }).then(res => { router.push('/') })
   }, [])
   return (
     <LoadingScreen />
